b6d155ec2a33c5be294d53267012888ab8f146aa
mitchelllambert <mal6@email.sc.edu>
2020-08-08 17:46:53 -0400

    Work on logic in loop

--- driver_observer.py
+++ driver_observer.py
@@ -1,16 +1,21 @@
-from observer2 import *
+from observer import *
 from Measurements import *
 import bme280
 import smbus2
 import connectDB
 import constants
-from time import sleep
+import time
 
-
+interval = 60 # used in loop
 port = 1
 address = 0x77
 bus = smbus2.SMBus(port)
 bme280.load_calibration_params(bus,address)
+
+startTimeInTicks = time.time()  # setting up variables for looping function
+localTime = time.localtime(time.time())
+globalSeconds = localTime[5]
+recording = 1
 
 def read_all():
     data = bme280.sample(bus,address)
@@ -20,12 +25,17 @@ def read_all():
     measurements = [h.getValue(),t.getValue(),p.getValue()]
     return measurements
 
+def startOfMinute(seconds):
+    if globalSeconds == 0:
+        return True
+    else:
+        return False
 
 connection = connectDB.establish_connection()
 cursor = connectDB.setCursor(connection)
 publisher = Publisher()
 
-display = Subscriber('I, the compiler')
+display = Subscriber('The compiler')
 db = SubscriberDB('MariaDB', connection, cursor)
 # visualization = SubscriberVisualization('Python Graph')
 # interface = SubscriberAlexa('MQQT Broker')
@@ -36,10 +46,22 @@ publisher.register(db, db.insert)
 #  publisher.register(visualization, visualization.graphicallyDisplay)
 #  publisher.register(interface, interface.transferDataToBroker)
 
-print("Obtaining first reading::")
-measurements = read_all()
-publisher.dispatch(measurements)
-print("Obtaining second reading::")
-measurements = read_all()
-publisher.dispatch(measurements)
 
+retBool = startOfMinute(globalSeconds)
+while retBool is False:
+    localTime = time.localtime(time.time())
+    globalSeconds = localTime[5]
+    retBool = startOfMinute(globalSeconds)
+
+else:
+    while recording < 10:
+        localTime = time.localtime(time.time())
+        globalSeconds = localTime[5]
+        if globalSeconds % 10 == 0:
+            retList = read_all()
+            print("Obtained recording " + str(recording) + " on second " + str(globalSeconds) + " of current minute...")
+            print()
+            publisher.dispatch(retList)
+            recording += 1
+        elif recording == 10:
+            print("Recorded 10 measurements")
